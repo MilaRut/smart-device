58c6c460344d53705a3722fce655cbe2963a1c00
Mila Rut <mila.balashova2312@gmail.com>
2022-11-09 04:16:11 +0300

    fixed issue with accessibility in accordion and buttons

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -2,15 +2,28 @@ function initAccordion() {
 
   const accBtns = document.querySelectorAll('.accordion__btn');
   const accBodys = document.querySelectorAll('.accordion__body');
+  const listItems = document.querySelectorAll('.accordion__item');
 
   accBodys.forEach((item) => item.classList.remove('no-js'));
   accBtns.forEach((item) => item.classList.remove('no-js'));
 
+  function setTabindex() {
+    listItems.forEach((listItem) => listItem.setAttribute('tabindex', '-1'));
+  }
+
+  function removeTabindex() {
+    listItems.forEach((listItem) => listItem.removeAttribute('tabindex'));
+  }
+
+  if (screen.width < 768) {
+    setTabindex();
+  } else {
+    removeTabindex();
+  }
+
   accBtns.forEach((item) => {
     item.addEventListener('click', () => {
-
       let content = item.nextElementSibling;
-
       if (content.style.maxHeight && item.classList.contains('open')) {
         accBtns.forEach(function (el) {
           el.classList.remove('open');
@@ -18,6 +31,7 @@ function initAccordion() {
         accBodys.forEach(function (el) {
           el.style.maxHeight = null;
         });
+        setTabindex();
       } else {
         accBtns.forEach(function (el) {
           el.classList.remove('open');
@@ -27,6 +41,7 @@ function initAccordion() {
           el.style.maxHeight = null;
           content.style.maxHeight = content.scrollHeight + 'px';
         });
+        removeTabindex();
       }
     });
   });
